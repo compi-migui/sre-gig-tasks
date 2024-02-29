e5e6c5706cda6abb5daacfc97357c69902cb2a76
Miguel Garcia <migarcia@redhat.com>
2024-02-29 16:16:56 +0100

    pod-watcher: restrict RBAC permissions
    Still using a ClusterRole so that we can watch arbitrary namespaces, as
    defined at runtime.

--- task5/pod-watcher/controllers/pod_controller.go
+++ task5/pod-watcher/controllers/pod_controller.go
@@ -35,9 +35,8 @@ type PodReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=my.domain,resources=pods,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=my.domain,resources=pods/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=my.domain,resources=pods/finalizers,verbs=update
+//+kubebuilder:rbac:groups=my.domain,resources=pods,verbs=get;list;watch
+//+kubebuilder:rbac:groups=my.domain,resources=pods/status,verbs=get
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
